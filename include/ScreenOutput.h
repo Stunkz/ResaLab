d4447bc3736ab169cb0a1d25170b4beb46ce1c60
Stunkz <ludovicracot65@gmail.com>
2025-06-02 17:42:22 +0200

    Real Screen Library

---
+++ include/ScreenOutput.h
@@ -0,0 +1,12 @@
+#ifndef SCREENOUTPUT_H
+#define SCREENOUTPUT_H
+
+#define ENABLE_GxEPD2_GFX 0
+
+#include <GxEPD2_BW.h>
+#include <GxEPD2_3C.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+
+
+
+#endif // SCREENOUTPUT_H
--- lib/WeActStudio.EpaperModule/readme.md
+++
@@ -1,5 +0,0 @@
-# WeAct Studio Epaper Module
-!["Epaper Module 2.13"](./Images/1.png )
-!["Epaper Module 2.9"](./Images/2.png )
-!["Epaper Module 1.54"](./Images/3.png )
-!["Epaper Module 4.2"](./Images/4.png )
